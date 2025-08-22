0b85f5b509904e8ad55bee1209f272f6fd873697
lupones <lupones@disca.upv.es>
2025-08-22 11:58:36 +0200

    General config file to be coustomized to user machine

---
+++ configs.sh
@@ -0,0 +1,12 @@
+
+# Set this to point to the top level of the Tailbenchplusplus data directory
+DATA_ROOT=/home/USER_NAME/Tailbencbplusplus/tailbench.inputs
+
+# Set this to point to the top level installation directory of the Java
+# Development Kit. Only needed for Specjbb
+JDK_PATH=/usr/lib/jvm/java-8-openjdk-amd64
+
+# This location is used by applications to store scratch data during execution.
+SCRATCH_DIR=/home/USER_NAME/Tailbenchplusplus/pathtoscratch
+
+
